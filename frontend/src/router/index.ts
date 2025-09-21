e44e2c3f04e7269f67cf8996834e128899055d6b
Kalito <mixtape.0x56@gmail.com>
2025-09-21 11:30:18 -0500

    removed about page

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,13 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ChatWorkspace from '../components/chat/ChatWorkspace.vue'
 import PersonaManager from '../components/personas/PersonaManager.vue'
-import AboutView from '../views/AboutView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/kalito', name: 'chat', component: ChatWorkspace },
   { path: '/personas', name: 'personas', component: PersonaManager },
-  { path: '/about', name: 'about', component: AboutView },
 ]
 
 const router = createRouter({
