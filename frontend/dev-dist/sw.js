faf7d69815bbbf652c68df637c4f869a072b4611
Kalito <mixtape.0x56@gmail.com>
2025-11-04 13:19:59 -0600

    checkpoint before PWA setup

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ae867c42'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fcjptbn49q"
+    "revision": "0.4jbomn2efdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
