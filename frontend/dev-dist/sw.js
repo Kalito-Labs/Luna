b143be60a21c25bb13696e1c90a410bd4596aeab
Kalito <mixtape.0x56@gmail.com>
2025-11-08 17:01:23 -0600

    updated meds

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8b0af05'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.celrfmgcglo"
+    "revision": "0.8kuk39c8hc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
