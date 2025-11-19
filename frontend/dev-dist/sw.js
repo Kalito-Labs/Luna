2fe766a8faffc12b70e32d34a2d342113f9cf0cc
Kalito <mixtape.0x56@gmail.com>
2025-11-19 08:07:45 -0600

    Feature complete

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ae867c42'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.595qvpnlg38"
+    "revision": "0.jbr8oel9a2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
